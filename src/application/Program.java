9966d5f8a17184d254cd000b3856d65aba2f2838
Antonio Pires <tonan@globo.com>
2020-10-21 20:38:19 -0300

    Ainda batendo cabeça

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
@@ -14,6 +15,7 @@ public class Program {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		List<String> list = new ArrayList<>();
 		
 		System.out.print("Enter a file path: ");
 		String filePath=sc.nextLine();
@@ -24,36 +26,44 @@ public class Program {
 			
 			String line = br.readLine();
 			while (line!=null) {
-				returnMarkers(line);
+				String[] fields=returnMarkers(line);
+				
+				for (int i=0;i<fields.length;i++) {
+						list.add(fields[i]);	/*List<Piece> opponentPieces = piecesOnTheBoard
+						.stream()
+						.filter(x -> ((ChessPiece) x).getColor() == opponent(color))
+						.collect(Collectors.toList());*/
+			
+				}
+				
+				for (String fieldLine : list) {
+					for (int i=0;i<fields.length;i++) {
+						
+					}
+					List<String> filtered = list.stream().filter(x-> x!= fields.length ).collect(Collectors.toList());
+				}
+				//<List>String filtered = list.stream().filter(x-> ).collect(Collectors.toList());
 				
 				
+				br.readLine();
 				
 				
 			}
 			
 		}
 		
-
+		catch {
+			
+		}
 		sc.close();
 	}
-	
-	List<String> returnMarkers(String line) {
-		String[] markers=line.split("{[a-zA-Z]}");
-		
-		
-		for (int i=0;i<field.length;i++) {
-			String[] field=line.split("{");
-			if (field[i].indexOf('{') {
-				
-			}
-			String validField=field[i]+
-		}
-		
-		
-		
-		
-	// String[] ops = str.split("\\s*[a-zA-Z]+\\s*");	
-		
+
+	static String[] returnMarkers(String line) {
+		String[] markers = line.split("{[a-zA-Z]}");
+
+		return markers;
+		// String[] ops = str.split("\\s*[a-zA-Z]+\\s*");
+
 	}
 
 }
